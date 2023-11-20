5dac35564dce3b85e7701b7f1333321d980e19e8
1 <wrasf175@naver.com>
2023-11-20 17:49:06 +0900

    feat: 리뷰 목록 렌더링 기능 추가

--- src/views/ListDetail.vue
+++ src/views/ListDetail.vue
@@ -9,6 +9,7 @@
     const gymData = ref({});
     const reviewData = ref([]);
     const title = ref("제목");
+    const hasComment = ref(true);
 
     onBeforeMount(async () => {
         try {
@@ -30,8 +31,9 @@
     });
 
     onMounted(() => {
-
     });
+
+
 </script>
 
 <template>
@@ -67,9 +69,58 @@
             </div>
             <div class="pill_badge_container">
                 <h2>방문 후기</h2>
+                <div>
+                    <div id="badge">
+                        👶 초보자들이 입문하기좋아요
+                    </div>
+                    <div id="badge">
+                        🙆‍♂️ 직원이 친절해요
+                    </div>
+                    <div id="badge">
+                        📷 삼각대 보유중
+                    </div>
+                    <div id="badge">
+                        ♥ 시설이 청결해요
+                    </div>
+                    <div id="badge">
+                        🕋 시설이 넓어요
+                    </div>
+                </div>
             </div>
             <div class="review_container">
-
+                <h2>고객 리뷰</h2>
+                <div class="review_content">
+                    <div class="review_card">
+                        <div class="header_section">
+                            <img src="@/assets/profile.svg" alt="댓글 작성자">
+                            <p class="review_writer">닉네임</p>
+                            <p>방문일자 : 23.11.16</p>
+                        </div>
+                        <p class="review_content">다른 지점에 비해 넓고 좋네요.
+                            직원분들 친절하셔서 모르는 문제 잘 알려주시고 휴식공간도 쾌적해서 시간보내기 좋습니다. 김남준 사랑해...</p>
+                        <div v-if="hasComment" class="btn_section">
+                            <div class="btn_comment">댓글 1 개 더보기</div>
+                        </div>
+                    </div>
+                    <div class="review_card">
+                        <div class="header_section">
+                            <img src="@/assets/profile.svg" alt="댓글 작성자">
+                            <p class="review_writer">닉네임</p>
+                            <p>방문일자 : 23.11.16</p>
+                        </div>
+                        <p class="review_content">다른 지점에 비해 넓고 좋네요.
+                            직원분들 친절하셔서 모르는 문제 잘 알려주시고 휴식공간도 쾌적해서 시간보내기 좋습니다. 김남준 사랑해...</p>
+                    </div>
+                    <div class="review_card">
+                        <div class="header_section">
+                            <img src="@/assets/profile.svg" alt="댓글 작성자">
+                            <p class="review_writer">닉네임</p>
+                            <p>방문일자 : 23.11.16</p>
+                        </div>
+                        <p class="review_content">다른 지점에 비해 넓고 좋네요.
+                            직원분들 친절하셔서 모르는 문제 잘 알려주시고 휴식공간도 쾌적해서 시간보내기 좋습니다. 김남준 사랑해...</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -82,6 +133,7 @@
     width:100%;
     margin: 0 auto;
     height: 94vh;
+    overflow: scroll;
 
     color:white;
     background-color:#292929;
@@ -148,10 +200,97 @@
     margin-bottom: 1.6rem;
 
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     justify-content: space-between;
-    align-items: center;
+    align-items: start;
+    margin-left: 1.6rem;
 }
 
+#badge {
+    background-color: #36DDAB;
+    padding: 1rem;
+    border-radius: 0.5rem;
+    white-space: nowrap;
+
+    width: 100%;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: start;
+    margin-left: 0.5rem;
+    margin-bottom: 0.8rem;
+
+    display: inline-block;
+    width: auto;
+    white-space: nowrap;
+}
+
+.review_container{
+    width: 32rem;
+    height: 100%;
+    margin-top: 1.6rem;
+    margin-left: 1.6rem;
+    margin-bottom: 1.6rem;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: start;
+}
+
+.review_content{
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: start;
+}
+
+.review_card{
+    width: 100%;
+    height: 100%;
+
+    background-color: #1C1C1C;
+    border-radius: 0.5rem;
+    padding: 2rem;
+
+    margin-bottom: 1.6rem;
+}
+
+.header_section{
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: start;
+
+    font-size: 1.3rem;
+}
+.header_section img {
+    width: 2.4rem;
+    height: 2.4rem;
+
+    margin: 0.5rem;
+}
+
+.review_writer{
+    margin: 1rem;
+    flex-grow: 2;
+}
+
+.review_content{
+    font-size: 1.3rem;
+}
+
+.btn_section{
+    margin-top: 1.8rem;
+    font-size: 1.6rem;
+    text-align: center;
+}
 
 </style>
