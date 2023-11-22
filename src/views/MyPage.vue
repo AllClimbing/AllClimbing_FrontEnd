576c7fc5d39aaa8a3809f87331d7c44abe357fba
1 <wrasf175@naver.com>
2023-11-22 09:35:04 +0900

    style : 로그인 페이지 디자인 변경

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -1,9 +1,21 @@
 <script setup>
+    import { useRouter } from 'vue-router';
+
+    const router = useRouter();
+
+
+    const deleteLoginToken = () => {
+        localStorage.removeItem('access-token');
+        alert('로그아웃 되었습니다.');
+        router.push('/login');
+    }
 </script>
 
 <template>
     <div class="content-container">
         마이페이지 입니다.
+
+        <button @click="deleteLoginToken">로그아웃</button>
     </div>
 </template>
 
