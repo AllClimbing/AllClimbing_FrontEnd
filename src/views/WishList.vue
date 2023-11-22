01219e3bd52f47478382d863eef5a44e0567b843
jseok0917 <jayseok423@gmail.com>
2023-11-23 04:14:19 +0900

    fix : 자주가는 암장목록 조회 기능 디버깅

--- src/views/WishList.vue
+++ src/views/WishList.vue
@@ -7,6 +7,7 @@ import axios from 'axios';
 const router = useRouter();
 const listCnt = ref(0);
 const location = ref('');
+const gymIdList = ref([]);
 const gymList = ref([]);
 const wishList = ref([]);
 let isLoading = ref(true);
@@ -26,9 +27,15 @@ onBeforeMount(async() => {
             const token = localStorage.getItem('access-token').split('.')
             const id = JSON.parse(atob(token[1]))['userId'];
 
+            const gymResponse = await axios.get(`http://localhost:8080/api/gym/list/${id}`);
+            gymList.value = gymResponse.data;
+            console.log(gymList.value);
+
             const res = await axios.get(`http://localhost:8080/api/gym/list/${id}`);
             wishList.value = res.data;
             isLoading.value = false;
+
+
     } catch (e) {
         console.error("데이터 로딩에 실패했습니다");
     }
@@ -50,15 +57,15 @@ onBeforeMount(async() => {
             <div class="gym_list">
                 <div  
                     class="gym_content_container"
-                    v-for="gym in wishList"
-                    :key="gym.place_name"
+                    v-for="gym in gymList"
+                    :key="gym.gymName"
                     @click="routeToDetail(gym.id)"
                 >
                     <div class="gym-info__logo"></div>
                     <div class="gym-info__content">
-                        <div class="gym-info__content__name">{{gym.place_name}}</div>
-                        <div class="gym-info__content__address">{{gym.address_name}}</div>
-                        <div class="gym-info__content__phone">{{gym.phone ? gym.phone : "전화번호를 제공하지 않는 암장입니다."}}</div>
+                        <div class="gym-info__content__name">{{gym.gymName}}</div>
+                        <div class="gym-info__content__address">{{gym.gymAddress}}</div>
+                        <div class="gym-info__content__phone">{{gym.contact ? gym.contact : "전화번호를 제공하지 않는 암장입니다."}}</div>
                       </div>
                       <div class="gym-info__content__distance">
 
